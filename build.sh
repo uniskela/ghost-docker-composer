da90072348d0b1f32c2a249ce285f37823b012b9
Uniskela <alex@uniskela.space>
2023-09-10 01:59:21 +1000

    Fixed template.yml edit error with sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 clear
 bold=$(tput bold)
 underline=$(tput smul)
-italic=$(tput sitm)
 info=$(tput setaf 2)
 error=$(tput setaf 160)
 warn=$(tput setaf 214)
@@ -10,8 +9,10 @@ reset=$(tput sgr0)
 
 # Welcome message
 echo "----------------------------------------------------"
-echo "Welcome to the Ghost blog Docker composer!"
-echo "${italic}Built by Uniskela: https://github.com/uniskela/ghost-docker-composer${reset}"
+echo "${bold}Welcome to the Ghost blog Docker composer!${reset}"
+echo "${underline}Built by Uniskela: https://github.com/uniskela/ghost-docker-composer${reset}"
+echo ""
+echo "${error}Please note: This project is currently in development. Expect errors.${reset}"
 echo "----------------------------------------------------"
 sleep 5
 
@@ -22,13 +23,13 @@ echo "----------------------------------------------------"
 echo "Checking Docker installation...."
 sleep 2
 if ! command -v docker &> /dev/null; then
-    echo "Docker Engine is not installed."
+    echo "${error}Docker Engine is not installed.${reset}"
     echo "Please follow Docker's installation instructions:"
     echo "https://docs.docker.com/engine/install/ubuntu/#installation-methods"
     echo "----------------------------------------------------"
     exit 1
 else
-    echo "Docker is installed."
+    echo "${info}Docker is installed.${reset}"
 fi
 
 # Check if direnv is installed
@@ -36,7 +37,7 @@ echo "----------------------------------------------------"
 echo "Checking direnv installation...."
 sleep 2
 if ! command -v direnv &> /dev/null; then
-    echo "direnv is not installed."
+    echo "${warn}direnv is not installed.${reset}"
     echo "Would you like to install it now (via sudo apt-get)? (y/n)"
     read -r confirm
     if [ "$confirm" == "y" ]; then
@@ -51,26 +52,33 @@ if ! command -v direnv &> /dev/null; then
         else
             echo "$HOME/.bashrc does not exist"
         fi
-        echo "direnv installed successfully!"
+        echo "${info}direnv installed successfully!${reset}"
     else
+        echo "${error}direnv is not installed.${reset}"
         echo "Please follow direnv's installation instructions:"
         echo "https://direnv.net/docs/installation.html"
         echo "----------------------------------------------------"
         exit 1
     fi
 else
-    echo "direnv is installed."
+    echo "${info}direnv is installed.${reset}"
     echo "----------------------------------------------------"
 fi
-
-
-
-
-# Add checks for other packages here...
-
-echo "Done! Continuing on..."
-echo "----------------------------------------------------"
-sleep 5
+# Check if netstat is installed
+echo "Checking for netstat...."
+sleep 2
+# Check if netstat is installed
+if ! command -v netstat &> /dev/null
+then
+    echo "netstat could not be found. Installing net-tools..."
+    # Update package lists
+    sudo apt-get update
+    # Install net-tools
+    sudo apt-get install -y net-tools
+fi
+    echo "${info}net-tools is installed.${reset}"
+    echo "----------------------------------------------------"
+sleep 3 
 clear
 
 
@@ -80,8 +88,7 @@ echo "----------------------------------------------------"
 warn=$(tput setaf 214)
 reset=$(tput sgr0)
 echo "${warn}##!!Please be aware!!##${reset}"
-echo "This will remove any existing docker-compose.yml and"
-echo "prod/.envrc files from this directory."
+echo "This will remove any existing docker-compose.yml and prod/.envrc files from this directory."
 read -r -p "Are you sure you want to continue? [y/N] " response
 echo "----------------------------------------------------"
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
@@ -108,9 +115,9 @@ then
     touch ./prod/.envrc
     sleep 1
 
-    echo "Done! Your docker-compose.yml and prod/.envrc files have been refreshed."
+    echo "${info}Done! Your docker-compose.yml and prod/.envrc files have been refreshed.${reset}"
 else
-    echo "Operation cancelled. Your docker-compose.yml and prod/.envrc files have not been changed."
+    echo "${error}Operation cancelled. Your docker-compose.yml and prod/.envrc files have not been changed.${reset}"
     echo "----------------------------------------------------"
     exit 1
 fi
@@ -128,7 +135,7 @@ prompt_and_confirm() {
     # Check if the environment variable is already set
     if grep -q "$env_var_name" prod/.envrc; then
         echo "----------------------------------------------------"
-        echo "The environment variable $env_var_name is already set to $(grep "$env_var_name" prod/.envrc | cut -d '=' -f2-)."
+        echo "${warn}The environment variable $env_var_name is already set to $(grep "$env_var_name" prod/.envrc | cut -d '=' -f2-).${reset}"
         echo "Would you like to keep this value? (y/n, or press Enter to confirm)"
         read -r confirm
         if [ "$confirm" == "y" ] || [ -z "$confirm" ]; then
@@ -147,7 +154,7 @@ prompt_and_confirm() {
         if [ "$confirm" == "y" ] || [ -z "$confirm" ]; then
             echo "----------------------------------------------------"
             echo "export $env_var_name=$var" >> prod/.envrc
-            echo "$env_var_name set successfully!"
+            echo "${info}$env_var_name set successfully!${reset}"
             echo "----------------------------------------------------"
             sleep 3
             clear
@@ -160,10 +167,10 @@ prompt_and_confirm() {
 
 clear
 # Prompt for Ghost Image Version
-prompt_and_confirm "Ghost Image Version" "GHOST_IMAGE_VERSION"
 echo "----------------------------------------------------"
-echo "To ensure you enter the correct value, please" 
-echo "refer to: https://hub.docker.com/_/ghost/tags"
+echo "${warn}To ensure you enter the correct value, please" 
+echo "refer to: https://hub.docker.com/_/ghost/tags${reset}"
+prompt_and_confirm "Ghost Image Version" "GHOST_IMAGE_VERSION"
 
 # Replace the entire line 4 with the new image line
 sed -i "5c\    image: ghost:${GHOST_IMAGE_VERSION}" ./docker-compose.yml
@@ -171,76 +178,72 @@ sed -i "5c\    image: ghost:${GHOST_IMAGE_VERSION}" ./docker-compose.yml
 # Prompt for Ghost Website URL and create Docker secret
 prompt_and_confirm "Ghost Website URL" "database__connection__host"
 
-# Prompt for MySQL configuration type
-echo "----------------------------------------------------"
-echo "MySQL Configuration"
-echo "----------------------------------------------------"
-echo "Please choose the type of MySQL configuration:"
-echo "1. Internal"
-echo "2. External"
-read -r -p "Enter your choice (1 or 2): " choice
-
-if [[ "$choice" == "1" ]]; then
-    # Uncomment MySQL service and volume in template.yml using sed
-    sed -i '/#  mysql:/,/  mysql_data:/ { s/^#  // }' ./template.yml
-    sed -i '/#  mysql_data:/ { s/^#  // }' ./template.yml
-
-    # Add the necessary indentation back in
-    sed -i '/mysql:/,/mysql_data:/ { s/^/  / }' ./template.yml
-
-    # Correct the indentation for the mysql_data volume
-    sed -i '/mysql_data:/ { s/^/  / }' ./template.yml
-fi
-
-
-    # Prompt for internal MySQL configuration
+while true; do
+    # Prompt for MySQL configuration type
     echo "----------------------------------------------------"
-    echo "Internal MySQL Configuration"
+    echo "${underline}MySQL Configuration${reset}"
     echo "----------------------------------------------------"
-    prompt_and_confirm "MySQL Root Password" "MYSQL_ROOT_PASSWORD"
-    prompt_and_confirm "MySQL Database" "MYSQL_DATABASE"
-    # Record the database name for the ghost database connection
-    export DATABASE_CONNECTION_DATABASE=$MYSQL_DATABASE
-    prompt_and_confirm "MySQL User" "MYSQL_USER"
-    # Record the user for the ghost database connection
-    export DATABASE_CONNECTION_USER=$MYSQL_USER
-    prompt_and_confirm "MySQL Password" "MYSQL_PASSWORD"
-    # Record the password for the ghost database connection
-    export DATABASE_CONNECTION_PASSWORD=$MYSQL_PASSWORD
-    # Set the hostname and port for the ghost database connection
-    export DATABASE_CONNECTION_HOST="mysql"
-    export DATABASE_CONNECTION_PORT="3306"
-elif [[ "$choice" == "2" ]]; then
-    # Prompt for external MySQL configuration
-    echo "----------------------------------------------------"
-    echo "External MySQL Configuration"
-    echo "----------------------------------------------------"
-    prompt_and_confirm "Database Connection Hostname" "DATABASE_CONNECTION_HOST"
-    prompt_and_confirm "Database Connection Username" "DATABASE_CONNECTION_USER"
-    prompt_and_confirm "Database Connection DB Name" "DATABASE_CONNECTION_DATABASE"
-    prompt_and_confirm "Database Connection Password" "DATABASE_CONNECTION_PASSWORD"
-    prompt_and_confirm "Database Connection Port" "DATABASE_CONNECTION_PORT"
-else
-    echo "Invalid choice. Please enter 1 for Internal or 2 for External."
-    exit 1
-fi
+    echo "Please choose the type of MySQL configuration:"
+    echo "1. Internal"
+    echo "2. External"
+    read -r -p "Enter your choice (1 or 2): " choice
 
+    if [[ "$choice" == "1" ]]; then
+        # Uncomment MySQL service and volume in docker-compose.yml using sed
+        sed -i '/#  mysql:/,/  mysql_data:/ { s/^#  // }' ./docker-compose.yml
+        sed -i '/#  mysql_data:/ { s/^#  // }' ./docker-compose.yml
+
+        # Add the necessary indentation back in
+        sed -i '/mysql:/,/mysql_data:/ { s/^/  / }' ./docker-compose.yml
+
+        # Correct the indentation for the mysql_data volume
+        sed -i '/mysql_data:/ { s/^/  / }' ./docker-compose.yml
+
+        # Prompt for internal MySQL configuration
+        echo "----------------------------------------------------"
+        echo "${underline}Internal MySQL Configuration${reset}"
+        echo "----------------------------------------------------"
+        prompt_and_confirm "MySQL Root Password" "MYSQL_ROOT_PASSWORD"
+        prompt_and_confirm "MySQL Database" "MYSQL_DATABASE"
+        prompt_and_confirm "MySQL User" "MYSQL_USER"
+        prompt_and_confirm "MySQL Password" "MYSQL_PASSWORD"
+        # Add the DATABASE_CONNECTION_HOST and DATABASE_CONNECTION_PORT environment variables to prod/.envrc
+        echo "----------------------------------------------------"
+        echo "Including additional env variables for Ghost..."
+        echo "export DATABASE_CONNECTION_HOST=mysql" >> prod/.envrc
+        echo "export DATABASE_CONNECTION_PORT=3306" >> prod/.envrc
+        break
+    elif [[ "$choice" == "2" ]]; then
+        # Prompt for external MySQL configuration
+        echo "----------------------------------------------------"
+        echo "${underline}External MySQL Configuration${reset}"
+        echo "----------------------------------------------------"
+        prompt_and_confirm "Database Connection Hostname" "DATABASE_CONNECTION_HOST"
+        prompt_and_confirm "Database Connection Username" "DATABASE_CONNECTION_USER"
+        prompt_and_confirm "Database Connection DB Name" "DATABASE_CONNECTION_DATABASE"
+        prompt_and_confirm "Database Connection Password" "DATABASE_CONNECTION_PASSWORD"
+        prompt_and_confirm "Database Connection Port" "DATABASE_CONNECTION_PORT"
+        break
+    else
+        echo "Invalid choice. Please enter 1 for Internal or 2 for External."
+    fi
+done
+
+    echo "----------------------------------------------------"
+    echo "${info}Database Configuration Complete!${reset}"
+    echo "----------------------------------------------------"
+    sleep 2
+    clear
 
 # Prompt for SMTP configuration
 echo "----------------------------------------------------"
-echo "Email SMTP Configuration"
+echo "${underline}Email SMTP Configuration${reset}"
 echo "----------------------------------------------------"
 sleep 2
 # Prompt for SMTP configuration and create Docker secrets
 prompt_and_confirm "Email From Address" "mail_from"
 
-sleep 2
-clear
-
 prompt_and_confirm "Email SMTP host" "mail_options_host"
-
-sleep 2
-clear
 
 # Prompt for secure connection
 while true; do
@@ -267,25 +270,12 @@ clear
 # Prompt for SMTP port
 prompt_and_confirm "SMTP Port" "mail_options_port"
 
+echo "----------------------------------------------------"
+echo "${info}SMTP Configuration Complete!${reset}"
+echo "----------------------------------------------------"
 sleep 2
 clear
-# Check if netstat is installed
-echo "----------------------------------------------------"
-echo "Checking for netstat...."
-sleep 2
-# Check if netstat is installed
-if ! command -v netstat &> /dev/null
-then
-    echo "netstat could not be found. Installing net-tools..."
-    # Update package lists
-    sudo apt-get update
-    # Install net-tools
-    sudo apt-get install -y net-tools
-fi
-echo "Done! Continuing on..."
-echo "----------------------------------------------------"
-sleep 3 
-clear
+
 # Function to prompt for port number and check if it's open
 prompt_for_port() {
     while true; do
@@ -294,16 +284,18 @@ prompt_for_port() {
 
         netstat_output=$(netstat -tuln | grep "$PORT")
         if [[ -z $netstat_output ]]; then
-            echo "Port $PORT is open."
+            echo "${info}Port $PORT is open.${reset}"
             break
         else
-            echo "Error: Port $PORT is not open. Please try another port."
+            echo "${warn}Error: Port $PORT is not open. Please try another port."
         fi
     done
 }
 
+
 echo "----------------------------------------------------"
-echo "Preparing Ghost options..."
+echo "${underline}Ghost Setup Configuration${reset}"
+echo "----------------------------------------------------"
 sleep 2
 
 prompt_for_port
@@ -321,13 +313,13 @@ sleep 2
 # Check if 'ghost' container already exists
 ghost_container=$(docker ps -a --filter "name=ghost" --format '{{.Names}}')
 if [[ $ghost_container == *"ghost"* ]]; then
-    echo "The 'ghost' container already exists."
+    echo "${warn}The 'ghost' container already exists.${reset}"
     read -r -p "Would you like to stop and remove it? (y/n): " confirm
     if [[ "$confirm" == "y" ]]; then
         docker stop ghost
         docker rm ghost
     else
-        echo "Cannot proceed with the 'ghost' container already existing. Exiting."
+        echo "${error}Cannot proceed with the 'ghost' container already existing. Exiting.${reset}"
         exit 1
     fi
 fi
@@ -340,12 +332,12 @@ if [[ $ghost_volume == *"ghost_ghost_content"* ]]; then
     if [[ "$confirm" == "y" ]]; then
         docker volume rm ghost_ghost_content
     else
-        echo "Cannot proceed with the 'ghost_ghost_content' volume already existing. Exiting."
+        echo "${error}Cannot proceed with the 'ghost_ghost_content' volume already existing. Exiting.${reset}"
         exit 1
     fi
 fi
 
-echo "Done! Continuing on..."
+echo "${info}Done! Continuing on...${reset}"
 echo "----------------------------------------------------"
 sleep 3 
 clear
@@ -368,14 +360,20 @@ docker compose --env-file .envrc up -d --name ghost
 # Wait for a few seconds to let the stack start
 echo "Waiting for the container to start..."
 sleep 5
+clear
+
+echo "----------------------------------------------------"
+echo "Checking container deployment status"
+echo "----------------------------------------------------"
+sleep 2
 
 # Check if the stack has deployed
 stack_services=$(docker ps --filter "name=ghost")
 
 if [[ $stack_services == *"ghost"* ]]; then
-    echo "Stack has deployed successfully!"
+    echo "${info}Stack has deployed successfully!${error}"
 else
-    echo "Something went wrong. The stack did not deploy."
+    echo "${error}Something went wrong. The stack did not deploy.${reset}"
     echo "Please check the Docker logs for more information."
     echo "Use the command: docker logs ghost"
     exit 1
@@ -385,16 +383,17 @@ fi
 service_state=$(docker ps --filter "name=ghost" --format '{{.State}}')
 
 if [[ $service_state == *"Up"* ]]; then
-    echo "Containers are running successfully!"
+    echo "${info}Containers are running successfully!${reset}"
 else
-    echo "Something went wrong. The service is not running."
+    echo "${error}Something went wrong. The service is not running.${reset}"
     echo "Service state: $service_state"
     echo "Please check the Docker logs for more information."
     echo "Use the command: docker logs ghost"
+    exit 1
 fi
 
 # Provide next steps
-echo "Your Ghost blog is now running in Docker!"
+echo "${info}Your Ghost blog is now running in Docker!"
 echo "You can access it at the URL you provided during setup."
 echo "To manage your Docker stack, use the following commands:"
 echo "- To view the services in your stack, use: docker ps --filter \"name=ghost\""
